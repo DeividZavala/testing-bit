57d163f138b7c5cdfa86062726b9f00c62c291cd
David <crusedmoss@gmail.com>
2020-01-08 16:58:31 -0600

    format files again

--- src/components/product-list/index.js
+++ src/components/product-list/index.js
@@ -3,24 +3,24 @@ import "./styles.css"
 
 const ProductList = () => {
 
-    const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
-    useEffect(()=>{
-        fetch("https://rickandmortyapi.com/api/character").then(res => res.json()).then(res=> {
-            setCharacters(res.results)
-        }).catch(err => console.log(err))
-    },[]);
+  useEffect(()=>{
+    fetch("https://rickandmortyapi.com/api/character").then(res => res.json()).then(res=> {
+      setCharacters(res.results)
+    }).catch(err => console.log(err))
+  },[]);
 
-    return (
-        <section className="grid">
-            {characters.map((character,index) => (
-                <div key={index}>
-                    <img src={character.image} alt=""/>
-                    <h4>{character.name}</h4>
-                </div>
-            ))}
-        </section>
-    )
+  return (
+    <section className="grid">
+      {characters.map((character,index) => (
+        <div key={index}>
+          <img src={character.image} alt=""/>
+          <h4>{character.name}</h4>
+        </div>
+      ))}
+    </section>
+  )
 };
 
 export default ProductList;
